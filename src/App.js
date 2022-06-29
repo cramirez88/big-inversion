6b78c924e3ede7577b9fb19eb9293b830f1226a3
Christian Ramirez <christian.r0602@gmail.com>
2022-06-29 11:09:19 -0500

    PersonCard component was created using classes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import PersonCard from './components/PersonCard';
 
 function App() {
   return (
-<h1>Hey</h1>
+    <>
+    <PersonCard firstName={'Jane'} lastName={'Doe'} age={45} hairColor={'Black'}/>
+    <PersonCard firstName={'John'} lastName={'Smith'} age={88} hairColor={'Brown'}/>
+    <PersonCard firstName={'Millard'} lastName={'Fillmore'} age={50} hairColor={'Brown'}/>
+    <PersonCard firstName={'Maria'} lastName={'Smith'} age={62} hairColor={'Brown'}/>
+    </>
   );
 }
 
